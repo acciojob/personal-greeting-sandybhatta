141ec455914131a04049bd5bac7c69df6524c830
sandybhatta <sandipresponse256@gmail.com>
2024-08-22 19:18:42 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[name,setName]=useState('')
+  function greeting(e){
+      setName(e.target.value)
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      <label htmlFor={"inp"}>Enter your name:</label>
+       <input type={"text"} id={"inp"}onInput={greeting}/>
+       <p>Hello {name}!</p>
     </div>
   )
 }
